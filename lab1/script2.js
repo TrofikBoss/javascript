ab7a4d8e9681fe7611a3ebe07bd422b2a33a9ed7
TrofikBoss <118187409+TrofikBoss@users.noreply.github.com>
2026-05-05 14:59:09 +0300

    Update script2.js

--- lab1/script2.js
+++ lab1/script2.js
@@ -2,20 +2,22 @@
 
 document.querySelector("#form_count").onsubmit = (ev) => {
     ev.preventDefault();
-    let num = document.querySelector("#number").value;
+    let num = parseInt(document.querySelector("#number").value);
     let str1 = "На ветке сидит " + num + " ворон";
-    let last_number = String(num)[String(num).length - 1];
+    let last_number = num % 10;
+    let last_number2 = num % 100 - last_number;
     if (num == 1) {
         str1 += "а";
     } else if (num >= 2 && num <= 4) {
         str1 += "ы";
     } else if (num > 20) {
-        if (last_number == "1") {
-            str1 += "а";
-        } else if (last_number >= 2 && last_number <= 4) {
-            str1 += "ы";
+        if (last_number2 != 1) {
+            if (last_number == 1) {
+                str1 += "а";
+            } else if (last_number >= 2 && last_number <= 4) {
+                str1 += "ы";
+            }
         }
     }
-
     alert(str1);
 }
